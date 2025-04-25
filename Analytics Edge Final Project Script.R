92a883bee5d95f084479888135d77d7ce6fba544
Anthony Evelyn <anthonyevelyn@Anthonys-MacBook-Pro.local>
2025-04-25 15:08:03 -0400

    Has Logistic Regression model, and started to run a gradient boosted model. Will run a XGBoost model soon.

--- Analytics Edge Final Project Script.R
+++ Analytics Edge Final Project Script.R
@@ -23,6 +23,18 @@ head(obesity_SA)
 ##Adding an indicator column for obesity status
 obesity_SA$obese_ind = ifelse(obesity_SA$NObeyesdad %in% c("Obesity_Type_I", "Obesity_Type_II", "Obesity_Type_III"), 1, 0)
 
+#Turning categorical variables into factors
+obesity_SA$family_history_with_overweight = as.factor(obesity_SA$family_history_with_overweight)
+obesity_SA$CALC = as.factor(obesity_SA$CALC)
+obesity_SA$MTRANS = as.factor(obesity_SA$MTRANS)
+obesity_SA$Gender = as.factor(obesity_SA$Gender)
+obesity_SA$CAEC = as.factor((obesity_SA$CAEC))
+obesity_SA$obese_ind = as.factor(obesity_SA$obese_ind)
+obesity_SA$SMOKE = as.factor(obesity_SA$SMOKE)
+obesity_SA$SCC = as.factor(obesity_SA$SCC)
+obesity_SA$FAVC = as.factor(obesity_SA$FAVC)
+
+
 cor_data= subset(obesity_SA, )
 
 #Viewing the data to make sure the column worked
@@ -35,12 +47,29 @@ obesity_SA$SamplingTerm <- interaction(obesity_SA$SMOKE,obesity_SA$SCC)
 
 set.seed(10, sample.kind = "Rejection")
 idx <- createDataPartition(obesity_SA$SamplingTerm, p = 0.7, list = FALSE)
-idx
+# idx
 train <- obesity_SA[idx,1:18]
 test <- obesity_SA[-idx,1:18]
 
 #Comparing some summary stats across the data sets
+head(train)
 summary(train)
 summary(test)
+
+obesity_SA$gender_ind <- ifelse(obesity_SA$Gender == 'Male', 1, 0)
+head(obesity_SA)
+
+
+# # Plot correlation matrix
+# ggcorrplot(cor_mat, show.diag=FALSE, hc.order = TRUE)
+
+#Logistic Regression
+log.model = glm(obese_ind ~ . -NObeyesdad, data=train, family='binomial')
+
+summary(log.model)
+
+
+
+
 
 
